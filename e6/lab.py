08a1e531a378e06f28ce396e38d15b990e2b942f
anton <antje12@student.sdu.dk>
2022-11-04 10:32:35 +0100

    e9 done

--- e6/lab.py
+++ e6/lab.py
@@ -1,13 +1,13 @@
 import random as rand
 import math as math
 
-a_1 = 101427
-c_1 = 321 # Step
-m_1 = 2**16 # Maximum / Modulus
+a = 101427
+c = 321 # Step
+m = 2**16 # Maximum / Modulus
 
-a_2 = 65539
-c_2 = 0 # Step
-m_2 = 2**31 # Maximum / Modulus
+a_r = 65539
+c_r = 0 # Step
+m_r = 2**31 # Maximum / Modulus
 
 seed = 123456789 # Seed
 amount = 10000
@@ -31,14 +31,12 @@ def pyNative():
         result.append(r)
     return result
 
-input = input()
-
-resultNative = pyNative()
-resultLcm1 = lcm(a_1, c_1, m_1)
-resultLcm2 = lcm(a_2, c_2, m_2)
+result_Native = pyNative()
+result_Lcm = lcm(a, c, m)
+result_Randu_Lcm = lcm(a_r, c_r, m_r)
 
 print("Kolmogorov-Smirnov Test!")
-result = resultNative[0:100]
+result = result_Lcm[0:100]
 result.sort()
 N = len(result)
 
@@ -65,16 +63,92 @@ else:
 
 
 print("Chi-Squared Test!")
-result = resultNative
+result = result_Lcm
+uniform = result_Native
 result.sort()
+uniform.sort()
 N = len(result)
 
-Ks = []
+observed = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+  7: 0,
+  8: 0,
+  9: 0,
+  10: 0
+}
+expected = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+  7: 0,
+  8: 0,
+  9: 0,
+  10: 0
+}
 
 for index in range(0, N):
     R = result[index]
-    if 0 <= R and R < 0.1:
-        
+    if R < 0.1:
+        observed[1] += 1
+    elif R < 0.2:
+        observed[2] += 1
+    elif R < 0.3:
+        observed[3] += 1
+    elif R < 0.4:
+        observed[4] += 1
+    elif R < 0.5:
+        observed[5] += 1
+    elif R < 0.6:
+        observed[6] += 1
+    elif R < 0.7:
+        observed[7] += 1
+    elif R < 0.8:
+        observed[8] += 1
+    elif R < 0.9:
+        observed[9] += 1
+    elif R < 1:
+        observed[10] += 1
 
+    R = uniform[index]
+    if R < 0.1:
+        expected[1] += 1
+    elif R < 0.2:
+        expected[2] += 1
+    elif R < 0.3:
+        expected[3] += 1
+    elif R < 0.4:
+        expected[4] += 1
+    elif R < 0.5:
+        expected[5] += 1
+    elif R < 0.6:
+        expected[6] += 1
+    elif R < 0.7:
+        expected[7] += 1
+    elif R < 0.8:
+        expected[8] += 1
+    elif R < 0.9:
+        expected[9] += 1
+    elif R < 1:
+        expected[10] += 1
 
-# Compare to Uniform distribution!
+X = 0
+l = len(observed)
+for i in range(1, len(observed)+1):
+    O = observed[i]
+    E = expected[i]
+    X += ((O - E)**2)/E
+
+Xa = 124.342 #???
+
+if (X <= Xa):
+    print("Accept H0!")
+else:
+    print("Reject H0!")
