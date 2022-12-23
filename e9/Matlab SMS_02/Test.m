7065d400101ce5e6d156866baa1a60716df50ff2
anton <antje12@student.sdu.dk>
2022-12-23 16:01:58 +0100

    test

--- e9/Matlab SMS_02/Test.m
+++ e9/Matlab SMS_02/Test.m
@@ -1,8 +1,9 @@
 clc
 clear
 close all
-trainSeqNumber = 100;    %number of training sequences - Test with 50, 60, 70, 80, 90, 100
+trainSeqNumber = 50;    %number of training sequences - Test with 50, 60, 70, 80, 90, 100
 testSeqNumber = 5;      %number of testing sequences
+
 
 %*********** data generation starts **********************************
 % system model: transition matrices
@@ -13,6 +14,7 @@ A(:,:,1) = [0.8 0.1 0.1;
 A(:,:,2) = [0.1 0.8 0.1;
             0.1 0.1 0.8;
             0.8 0.1 0.1];   % action: shift to next
+
 % system model: emission matrices
 B1 = [0.90 0.05 0.05;
       0.05 0.90 0.05;
@@ -20,6 +22,7 @@ B1 = [0.90 0.05 0.05;
 B2 = [0.80 0.15 0.05;
       0.20 0.75 0.05;
       0.10 0.05 0.85];
+
 % system model: initial distribution/initial states
 pinit = [1; 0; 0];
 
@@ -28,27 +31,25 @@ for i = 1:trainSeqNumber
     L = round(100.*rand + 150);
     p_trans = 0.5;     % prob to chose trans action
     U{i} = (rand(L-1, 1) < p_trans) + 1;
-    [X{i}, Y{i}, Z{i}] = simulation(pinit, A, B1,B2, U{i});
+    [X{i}, Y{i}, Z{i}] = simulation(pinit, A, B1, B2, U{i});
 end
 %*********** data generation ends ************************************
 
 
 %*********** Model initialization starts *****************************
-
 % init param
-A_init = rand(3,3,2);
+%A_init = rand(3,3,2);
 
-% A_init(:,:,1) = [0.90 0.10 0.00;   
-%                  0.00 0.90 0.10;   
-%                  0.00 0.10 0.80];   
-% A_init(:,:,2) = [0.00 0.75 0.25;   
-%                  0.00 0.25 0.75;   
-%                  0.75 0.25 0.00];
+A_init(:,:,1) = [0.8 0.1 0.1;
+                0.1 0.8 0.1;
+                0.1 0.1 0.8]; 
+A_init(:,:,2) = [0.1 0.8 0.1;
+                0.1 0.1 0.8;
+                0.8 0.1 0.1];
 
 B_init1 = rand(3,3);
 B_init2 = rand(3,3);
 pinit_init = rand(3,1);
-
 %*********** Model initialization ends ******************************
 
 
@@ -68,6 +69,7 @@ B_lrn1
 B_lrn2
 %*********** Model training ends ************************************
 
+
 %*********** Model testing starts ************************************
 disp ('04 The diagnosis estimation:------------------- ') 
 %generating testting sequences
@@ -76,17 +78,17 @@ for i = 1:testSeqNumber
     L = round(100.*rand + 150);
     p_trans = 0.5;     % prob to chose trans action
     U2{i} = (rand(L-1, 1) < p_trans) + 1;
-    [X2{i}, Y2{i}, Z2{i}] = simulation(pinit, A, B1,B2, U2{i});
+    [X2{i}, Y2{i}, Z2{i}] = simulation(pinit, A, B1, B2, U2{i});
 end
+
 % applying the Viterbi algorithm
-[current_distributions] = viterbi_7(Y2,Z2,U2, pinit_lrn, A_lrn, B_lrn1,B_lrn2)
+[current_distributions] = viterbi_7(Y2,Z2,U2, pinit_lrn, A_lrn, B_lrn1, B_lrn2)
 
 % Finding maximum probable states
 [value,index] = max(current_distributions);
-
 disp ('05 The final statement::------------------- ')
 if any(isnan(current_distributions)) 
-    disp ('Not enough information in the data. Creat a new dataset or increase the data volume.')
+    disp ('!!!!!! Not enough information in the data. Creat a new dataset or increase the data volume.')
 elseif (index == 1)
     disp ('The system is in good health.')
 elseif (index == 2)
